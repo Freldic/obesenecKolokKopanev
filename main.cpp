90fba5bf48b8562d539270b83c3aaf7989ba6961
Peter <peterkolokvlog@gmail.com>
2023-01-09 11:53:11 +0100

    import mainu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+#include "server.h"
+#include "client.h"
+#include "cstring"
 
-using namespace std;
+int main(int argc, char * argv[]){
+    Server *server = new Server();
+    Client *client = new Client();
+    char * argument[argc];
+    argument[0] = argv[0];
+    for(int i = 2; i<=argc; i++){
+        argument[i-1] = argv[i];
+    }
 
-int main() {
-    cout << "llo, World!" << endl;
-    return 0;
+    if (strcmp(argv[1],"server") == 0){
+        cout << "1. argument : " << argv[1] << endl;
+        server->server(argc -1, argument);
+    }
+
+    if(strcmp(argv[1],"klient") == 0){
+        cout << "1. argument : " << argv[1] << endl;
+        client->client(argc -1, argument);
+
+    } else {
+        cout << "Nastala chyba pri zadávaní argumentu na strane klienta!" << endl;
+        exit(1);
+    }
+
+    delete client;
+    delete server;
 }
